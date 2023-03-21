fe4b223c4cb648cf332c48eebc3fa813ba523329
orpd0523 <orpd0523@gmail.com>
2023-03-21 14:42:00 -0500

    created array with options

---
+++ scripts/scripts.js
@@ -0,0 +1,16 @@
+const chores = [
+  { category: "kitchen", chores: ["sweep", "stove top"] },
+  { category: "restroom", chores: ["sweep", "counter", "shower", "toilet"] },
+  { category: "room", chores: ["floor", "dust", "make bed"] },
+  { category: "outside", chores: ["work on hole", "clean trash", "organize"] },
+  { category: "car", chores: ["rinse", "clean trash out", "clean back"] },
+];
+
+//go through the options
+function select(chores){
+    
+}
+
+//make the options random
+//select an option
+//put option selected on the list
